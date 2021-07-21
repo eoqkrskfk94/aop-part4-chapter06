a1b0fe15a43214e0fc6970b22e0d8b92ff6a34f8
danielkim <37679062+eoqkrskfk94@users.noreply.github.com>
2021-07-21 23:45:47 +0900

    [Add] 실시간 대기 정보 불러오기

--- app/src/main/java/io/mobinity/wantact/aop_part4_chapter06/MainActivity.kt
+++ app/src/main/java/io/mobinity/wantact/aop_part4_chapter06/MainActivity.kt
@@ -1,23 +1,28 @@
-package io.mobinity.wantact.aop_part4_chapter06
+ package io.mobinity.wantact.aop_part4_chapter06
 
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.tasks.CancellationTokenSource
 import io.mobinity.wantact.aop_part4_chapter06.data.Repository
+import io.mobinity.wantact.aop_part4_chapter06.data.models.airquality.Grade
+import io.mobinity.wantact.aop_part4_chapter06.data.models.airquality.MeasuredValue
+import io.mobinity.wantact.aop_part4_chapter06.data.models.monitoringstation.MonitoringStation
 import io.mobinity.wantact.aop_part4_chapter06.databinding.ActivityMainBinding
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
 
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private var cancellationTokenSource: CancellationTokenSource? = null
@@ -32,6 +37,7 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        bindViews()
         initVariables()
         requestLocationPermissions()
 
@@ -63,6 +69,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+     private fun bindViews(){
+         binding.refresh.setOnRefreshListener {
+             fetchAirQualityData()
+         }
+     }
+
     private fun initVariables() {
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
     }
@@ -88,16 +100,73 @@ class MainActivity : AppCompatActivity() {
             cancellationTokenSource!!.token
         ).addOnSuccessListener { location ->
             scope.launch {
-                val monitoringStation =
-                    Repository.getNearbyMonitoringStation(location.latitude, location.longitude)
+                binding.errorDescriptionTextView.visibility = View.GONE
+                try {
+                    val monitoringStation =
+                        Repository.getNearbyMonitoringStation(location.latitude, location.longitude)
 
-                val measuredValue = Repository.getLatestAirQualityData(monitoringStation!!.stationName!!)
+                    val measuredValue = Repository.getLatestAirQualityData(monitoringStation!!.stationName!!)
 
-                binding.tv.text = measuredValue.toString()
+                    displayAirQuality(monitoringStation, measuredValue!!)
+                }catch (exception: Exception) {
+                    binding.errorDescriptionTextView.visibility = View.VISIBLE
+                    binding.contentsLayout.alpha = 0F
+                } finally {
+                    binding.progressBar.visibility = View.GONE
+                    binding.refresh.isRefreshing = false
+                }
 
 
             }
         }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun displayAirQuality(monitoringStation: MonitoringStation, measuredValue: MeasuredValue) {
+        binding.contentsLayout.animate()
+            .alpha(1F)
+            .start()
+
+        binding.measuringStationNameTextView.text = monitoringStation.stationName
+        binding.measuringStationAddressTextView.text = monitoringStation.addr
+
+        (measuredValue.khaiGrade ?: Grade.UNKNOWN).let { grade ->
+            binding.root.setBackgroundResource(grade.colorResId)
+            binding.totalGradeLabelTextView.text = grade.label
+            binding.totalGradleEmojiTextView.text = grade.emoji
+        }
+
+        with(measuredValue) {
+            binding.fineDustInformationTextView.text = "미세먼지: ${pm10Value} ㎍/㎥ ${(pm10Grade ?: Grade.UNKNOWN).emoji}"
+            binding.ultraFineDustInformationTextView.text = "초미세먼지: $pm25Value ㎍/㎥ ${(pm25Grade ?: Grade.UNKNOWN).emoji}"
+
+            with(binding.so2Item) {
+                labelTextView.text = "야황산가스"
+                gradeTextView.text = (so2Grade ?: Grade.UNKNOWN).toString()
+                valueTextView.text = "$so2Value ppm"
+            }
+
+            with(binding.coItem) {
+                labelTextView.text = "일산화탄소"
+                gradeTextView.text = (coGrade ?: Grade.UNKNOWN).toString()
+                valueTextView.text = "$coValue ppm"
+            }
+
+            with(binding.o3Item) {
+                labelTextView.text = "오존"
+                gradeTextView.text = (o3Grade ?: Grade.UNKNOWN).toString()
+                valueTextView.text = "$o3Value ppm"
+            }
+
+            with(binding.no2Item) {
+                labelTextView.text = "이산화질소"
+                gradeTextView.text = (no2Grade ?: Grade.UNKNOWN).toString()
+                valueTextView.text = "$no2Value ppm"
+            }
+
+        }
+
+
     }
 
     companion object {
